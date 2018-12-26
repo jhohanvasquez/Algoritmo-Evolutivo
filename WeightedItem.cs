fb2e637439f3a9f660df95ea48db873b48b48828
jhohan alexander vasquez <jhohan25@hotmail.com>
2018-12-26 16:03:33 -0500

    Cambios namespace

--- WeightedItem.cs
+++ WeightedItem.cs
@@ -1,11 +1,9 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace Algoritmo.Evolutivo.Consola
+﻿namespace Algoritmo.Evolutivo.Consola
 {
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+
     public class WeightedItem<T>
     {
         private T value;
