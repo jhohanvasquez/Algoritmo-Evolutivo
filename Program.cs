4df7a1231816f57e59bb5674a317c33511cf49c1
jhohan alexander vasquez <jhohan25@hotmail.com>
2018-12-26 15:45:33 -0500

    Agregar archivos de proyecto.

---
+++ Program.cs
@@ -0,0 +1,98 @@
+﻿namespace Algoritmo.Evolutivo.Consola
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+    using System.Text;
+    using System.Threading.Tasks;
+
+    class Program
+    {
+
+        #region constantes
+
+        const string CadenaB = "ESTO ES UN ALGORITMO EVOLUTIVO";
+        const string posibles = "ABCDEFGHIJKLMNOPQRSTUVWXYZ ";
+
+        #endregion
+
+        #region funciones
+
+        /// <summary>
+        /// Crea caracteres aleatorios con una longitud expecificada
+        /// </summary>
+        /// <param name="length"></param>
+        /// <returns></returns>
+        public static string CrearCaracter(int length)
+        {
+            Random obj = new Random();
+            int longitud = posibles.Length;
+            char letra;
+            int longitudnuevacadena = 1;
+            string nuevacadena = "";
+            for (int i = 0; i < longitudnuevacadena; i++)
+            {
+                letra = posibles[obj.Next(longitud)];
+                nuevacadena += letra.ToString();
+            }
+            return nuevacadena;
+        }
+
+        /// <summary>
+        /// Calificar la cadena generada
+        /// </summary>
+        /// <param name="cadenaA"></param>
+        /// <returns></returns>
+        public static int CalificarCadena(string cadenaA)
+        {
+            var resultado = 0;
+            var oCaracterA = cadenaA.ToCharArray();
+            var oCaracterB = CadenaB.ToCharArray();
+            for (int i = 0; i < oCaracterA.Length; i++)
+            {
+                if (oCaracterA[i].Equals(oCaracterB[i]))
+                {
+                    resultado++;
+                }
+            }
+
+            return resultado;
+
+        }
+
+        #endregion 
+
+        static void Main(string[] args)
+        {
+            var data = "RFXLIYGKDOUIWJKPIJAZAPSSNIPHYT";
+            var index = 0;
+            var mayorPuntaje = 0;
+            var cadenaMayor = string.Empty;
+            while (mayorPuntaje < 30)
+            {
+                for (int x = 0; x < 100; x++)
+                {
+                    var miLista = new List<WeightedItem<string>>();
+                    for (int i = 0; i < data.Length; i++)
+                    {
+                        WeightedItem<string> oItem = new WeightedItem<string>(i.ToString(), 1.5);
+                        miLista.Add(oItem);
+                    }
+                    var posicion = Convert.ToInt32(WeightedItem<string>.Choose(miLista));
+                    var newCaracter = CrearCaracter(1);
+                    data = data.Remove(posicion, 1).Insert(posicion, newCaracter);
+                    var auxPuntaje = CalificarCadena(data);
+                    if (mayorPuntaje < auxPuntaje)
+                    {
+                        mayorPuntaje = auxPuntaje;
+                        cadenaMayor = data;
+                    }
+                    Console.WriteLine($"Generación:{index++} - Mutación: {data} - Puntaje: {CalificarCadena(data)}");
+                }
+                data = cadenaMayor;
+            }
+            Console.WriteLine($"Generación:{index++} - Mutación: {data} - Puntaje: {CalificarCadena(data)}");
+            Console.ReadKey();
+        }
+    }
+}
---
+++ WeightedItem.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Algoritmo.Evolutivo.Consola
+{
+    public class WeightedItem<T>
+    {
+        private T value;
+        private double weight;
+        private double cumulativeSum;
+        private static Random rndInst = new Random();
+
+        public WeightedItem(T value, double weight)
+        {
+            this.value = value;
+            this.weight = weight;
+        }
+
+        public static T Choose(List<WeightedItem<T>> items)
+        {
+            double cumulSum = 0;
+            int cnt = items.Count();
+
+            for (int slot = 0; slot < cnt; slot++)
+            {
+                cumulSum += items[slot].weight;
+                items[slot].cumulativeSum = cumulSum;
+            }
+
+            double divSpot = rndInst.NextDouble() * cumulSum;
+            WeightedItem<T> chosen = items.FirstOrDefault(i => i.cumulativeSum >= divSpot);
+            if (chosen == null) throw new Exception("No item chosen - there seems to be a problem with the probability distribution.");
+            return chosen.value;
+        }
+    }
+
+}
